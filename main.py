88e0cb7887f6b14cfaeae3de10549b19fb10ea7a
Muddyblack <Muddyblack03@gmail.com>
2023-04-12 14:23:58 +0200

    refresh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from Functions import file_helper
 import os
-path = f"{os.path.dirname(os.path.abspath(__file__))}"
-print(path)
 
-txt = file_helper.read_file(f"{path}/Saving/brudddi.txt")
-print(txt)
+project_path = f"{os.path.dirname(os.path.abspath(__file__))}"
+print(project_path)
+
