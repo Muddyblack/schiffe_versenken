0f21ccbb228cfd3f799266ccbf37894cbd0a6680
Muddyblack <Muddyblack03@gmail.com>
2023-04-12 19:13:30 +0200

    show fiallld

--- Classes/Spielfeld.py
+++ Classes/Spielfeld.py
@@ -1,22 +1,38 @@
+from string import ascii_uppercase
+from random import randint
+
 class Spielfeld:
     def __init__(self, bot = False):
-        self.__hitfield = [[0] * 10] * 10
-        self.__boatfield = [[0] * 10] * 10
+        self.__fsize = 10 
+        self.__hitfield = [[0] * self.__fsize] * self.__fsize
+        self.__boatfield = [[0] * self.__fsize] * self.__fsize
         
         self.__bot = bot
         self.__ships = 10
 
-    def show_boatfield(self):
-        for l in self.__boatfield:
+    def show_field(self, fieldtype):
+
+        print(" " * (len(str(self.__fsize))+1), end = "")
+        for a in range(self.__fsize):
+            print(f"{ascii_uppercase[a]}", end = " ")
+
+        print("")
+
+        for num, l in enumerate(fieldtype, 1):
+            print(f"{num}", end="  ")
+
+            if(len(str(num)) > 1):
+                print("\b"*(len(str(num))-1), end = "")
+
             for r in l:
-                print(r, end="") 
+                print(r, end=" ") 
             print("")
 
+    def show_boatfield(self):
+        self.show_field(self.__boatfield)
+
     def show_hitfield(self):
-        for l in self.__hitfield:
-            for r in l:
-                print(r, end="") 
-            print("")    
+        self.show_field(self.__hitfield)
 
     def get_ships(self):
         return self.__ships
