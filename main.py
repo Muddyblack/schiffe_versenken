8f55f4fbc628ab98b0a7c868a6ae2d3f2c7984bf
kasfana <kasfana.contact@gmail.com>
2023-04-14 22:19:12 +0200

    test wegen git

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+"""
+Main Skript
+"""
+
 import os
-import Classes.game_field as game_field
+from Classes import game_field
 
 project_path = f"{os.path.dirname(os.path.abspath(__file__))}"
 
 
 def clear_previous_console_output():
+    """Löscht alles bisherige aus der Konsole"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
@@ -14,9 +19,8 @@ if __name__ == "__main__":
     s2 = game_field.GameField()
     s2.set_ship(5)
     s2.show_boatfield()
-
+    print("Das ist ein test für git")
     while True:
         s1.attack_enemy(s2)
         s1.show_hitfield()
         s2.show_boatfield()
-
