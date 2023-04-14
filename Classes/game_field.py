c472f7ee292ece0e0020012033a258fe625041b5
kasfana <kasfana.contact@gmail.com>
2023-04-14 21:11:35 +0200

    Konsole leeren hinzugefügt main start in main.py verschoben

--- Classes/game_field.py
+++ Classes/game_field.py
@@ -173,16 +173,3 @@ class GameField:
             print("We already hit this Part")
         else:
             print("Sir we've hit the bull's eye!")
-
-
-if __name__ == "__main__":
-    s1 = GameField(False)
-
-    s2 = GameField()
-    s2.set_ship(5)
-    s2.show_boatfield()
-
-    while True:
-        s1.attack_enemy(s2)
-        s1.show_hitfield()
-        s2.show_boatfield()
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import os
+import Classes.game_field as game_field
 
 project_path = f"{os.path.dirname(os.path.abspath(__file__))}"
-print(project_path)
+
+
+def clear_previous_console_output():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+if __name__ == "__main__":
+    s1 = game_field.GameField(False)
+
+    s2 = game_field.GameField()
+    s2.set_ship(5)
+    s2.show_boatfield()
+
+    while True:
+        s1.attack_enemy(s2)
+        s1.show_hitfield()
+        s2.show_boatfield()
+
