27c76c3f4c4b1ac9ff87fd1841aff095663e58b3
Muddyblack <Muddyblack03@gmail.com>
2023-04-17 08:09:55 +0200

    del useless file

--- sound.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-from Library import sound_helper
-
-# Open the WAV file
-file_path = f"{os.path.join(os.path.dirname(os.path.realpath(__file__)))}/GameData/sound/Start-Screen.wav"
-
-sound_helper.start(file_path)
-sound_helper.stop(file_path)
