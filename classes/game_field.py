6db89f531c4513f70546ff4f7eb43bb4d34f2544
Muddyblack <Muddyblack03@gmail.com>
2023-04-30 15:00:10 +0200

    Update game_field spelling mistakes

--- classes/game_field.py
+++ classes/game_field.py
@@ -70,12 +70,14 @@ class GameField:
         # Set Uppersite Alphabet
         txt = " " * (len(str(self.__fsize)) + 2)
         for elem in range(self.__fsize):
-            txt += f"{ascii_uppercase[elem]} "
+            txt += (
+                f"{console_helper.BROWN}{ascii_uppercase[elem]}{console_helper.RESET} "
+            )
         txt += "\n"
 
         # Set complete matrix with Linenumbers at the side
         for num, line in enumerate(fieldtype, 1):
-            txt += f"{num}   "
+            txt += f"{console_helper.BROWN}{num}{console_helper.RESET}   "
             if len(str(num)) > 1:
                 txt += "\b" * (len(str(num)) - 1)
             for row in line:
